0f541fc2dcd1f1458330f69df72c758baa8c3ee5
Harsh Mriduhash <Harshsahay2709@gmail.com>
2025-03-12 11:35:56 +0530

    Auth

--- src/Components/AuthForm.js
+++ src/Components/AuthForm.js
@@ -1,69 +1,92 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const AuthForm = () => {
-    const [isLogin, setLogin] = useState(true)
-    const [show, setShow] = useState(false)   //for password showhidepassword
-    const [data, setData] = useState(undefined)
+  const [isLogin, setLogin] = useState(true);
+  const [show, setShow] = useState(false); //for password showhidepassword
+  const [data, setData] = useState(undefined);
 
-    const options = [
-        "Admin",
-        "Supervisor",
-        "Manager",
-        "Production",
-        "Quality",
-        "Store",
-    ];
+  const options = [
+    "Admin",
+    "Supervisor",
+    "Manager",
+    "Production",
+    "Quality",
+    "Store",
+  ];
 
   return (
-    <div className='container'>
-        <div className='welcome'>
+    <div className="container">
+      <div className="welcome">
         <h1>PG Production report!</h1>
+      </div>
+      <div className="form-container">
+        <div className="form-toggle">
+          <button
+            className={isLogin ? "active" : ""}
+            onClick={() => setLogin(true)}
+          >
+            Login
+          </button>
+          <button
+            className={!isLogin ? "active" : ""}
+            onClick={() => setLogin(false)}
+          >
+            Signup
+          </button>
         </div>
-        <div className='form-container'>
-            <div className='form-toggle'>
-                <button className={isLogin ? 'active' : ""} onClick={()=>setLogin(true)}>Login</button>
-                <button className={!isLogin ? 'active' : ""} onClick={()=>setLogin(false)}>Signup</button>
+        {isLogin ? (
+          <>
+            <div className="form">
+              <h2>Login Form</h2>
+              <input type="email" placeholder="Email"></input>
+              <input type="Password" placeholder="Password"></input>
+              <a href="#">Forget Password?</a>
+              <button>Login</button>
+              <p>
+                Not a Member?{" "}
+                <a href="#" onClick={() => setLogin(false)}>
+                  Signup now
+                </a>
+              </p>
             </div>
-            {isLogin ? <>
-            <div className='form'>
-                <h2>Login Form</h2>
-                <input type='email' placeholder='Email'></input>
-                <input type='Password' placeholder='Password'></input>
-                <a href='#'>Forget Password?</a>
-                <button>Login</button>
-                <p>Not a Member? <a href='#' onClick={()=>setLogin(false)}>Signup now</a></p>
-            </div>
-            </> : <>
-            <div className='form'>
-                <h2>{data} Signup Form</h2>
-                <div className='select-box'>
-                    <select onClick={(e)=>setData(e.target.value)}>
-                    <option>Please choose one</option>
-                    {options.map((option, index)=>{
-                        return (
-                            <option key={index}>
-                                {option}
-                            </option>
-                        )
-                    })}
+          </>
+        ) : (
+          <>
+            <div className="form">
+              <h2>{data} Signup Form</h2>
+              <div className="select-box">
+                <select onClick={(e) => setData(e.target.value)}>
+                  <option>Please choose one</option>
+                  {options.map((option, index) => {
+                    return <option key={index}>{option}</option>;
+                  })}
                 </select>
-                </div>
-                <input type='text' placeholder='Name'></input>
-                <input type='email' placeholder='Email'></input>
-                <div>
-                <input type={show ? 'text' : 'password'} placeholder='Password' ></input>
-                <input type={show ? 'text' : 'password'} placeholder='confirm Password' ></input>
-                <button onClick={()=>setShow(!show)}>{show ? "Hide" : 'Show'}</button>
-                </div>
-              
-                
-                // eslint-disable-next-line jsx-a11y/anchor-has-content
-                <button>Submit <a href=''></a></button>
+              </div>
+              <input type="text" placeholder="Name"></input>
+              <input type="email" placeholder="Email"></input>
+              <div>
+                <input
+                  type={show ? "text" : "password"}
+                  placeholder="Password"
+                ></input>
+                <input
+                  type={show ? "text" : "password"}
+                  placeholder="confirm Password"
+                ></input>
+                <button onClick={() => setShow(!show)}>
+                  {show ? "Hide" : "Show"}
+                </button>
+              </div>
+              // eslint-disable-next-line jsx-a11y/anchor-has-content
+              <button>
+                Submit <a href=""></a>
+              </button>
             </div>
-            </>}
-        </div>
+          </>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AuthForm
+export default AuthForm;
