50003ec2d3f95d039be661211fa8c05907f9b8db
Harsh Mriduhash <Harshsahay2709@gmail.com>
2025-03-12 11:59:09 +0530

    CompanyPage

--- src/Components/CompanyPage.js
+++ src/Components/CompanyPage.js
@@ -1,30 +1,28 @@
-import React from 'react'
-import {Router, NavLink} from 'react-router-dom'
+import React from "react";
+import { Router, NavLink } from "react-router-dom";
 
 const CompanyPage = () => {
-
   return (
     <Router>
-    <div className='navbar'>
-      <div className='navbar-logo'>Website</div>
-      <ul>
-        <li>
-          <NavLink>Home</NavLink>
-        </li>
-        <li>
-          <NavLink></NavLink>
-        </li>
-        <li>
-          <NavLink></NavLink>
-        </li>
-        <li>
-          <NavLink></NavLink>
-        </li>
-      </ul>
-    </div>
+      <div className="navbar">
+        <div className="navbar-logo">Website</div>
+        <ul>
+          <li>
+            <NavLink>Home</NavLink>
+          </li>
+          <li>
+            <NavLink></NavLink>
+          </li>
+          <li>
+            <NavLink></NavLink>
+          </li>
+          <li>
+            <NavLink></NavLink>
+          </li>
+        </ul>
+      </div>
     </Router>
-    
-  )
-}
+  );
+};
 
-export default CompanyPage
+export default CompanyPage;
